4c680478975474c1d3b51360fc9a99a21d2ba15c
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-27 12:09:47 -0300

    Add model mapper

--- src/main/java/com/matheuscordeiro/libaryapi/api/resource/BookController.java
+++ src/main/java/com/matheuscordeiro/libaryapi/api/resource/BookController.java
@@ -3,6 +3,7 @@ package com.matheuscordeiro.libaryapi.api.resource;
 import com.matheuscordeiro.libaryapi.api.dto.BookDTO;
 import com.matheuscordeiro.libaryapi.model.entity.Book;
 import com.matheuscordeiro.libaryapi.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,23 +11,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
-    @Autowired
+
     private BookService bookService;
+    private ModelMapper modelMapper;
+
+    public BookController(BookService bookService, ModelMapper modelMapper) {
+        this.bookService = bookService;
+        this.modelMapper = modelMapper;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto){
-        Book entity = Book.builder()
-                        .title(dto.getTitle())
-                        .author(dto.getAuthor())
-                        .isbn(dto.getIsbn())
-                        .build();
+        Book entity = modelMapper.map(dto, Book.class);
         entity = bookService.save(entity);
-        return BookDTO.builder()
-                .id(entity.getId())
-                .title(entity.getTitle())
-                .author(entity.getAuthor())
-                .isbn(entity.getIsbn())
-                .build();
+        return modelMapper.map(entity, BookDTO.class);
     }
 }
