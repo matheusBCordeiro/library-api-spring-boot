cbbebb3d0c744c4e5f0df11d603d256b21c1c151
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-29 12:31:43 -0300

    Post create loan

--- src/main/java/com/matheuscordeiro/libaryapi/api/resource/LoanController.java
+++ src/main/java/com/matheuscordeiro/libaryapi/api/resource/LoanController.java
@@ -1,4 +1,33 @@
 package com.matheuscordeiro.libaryapi.api.resource;
 
+import com.matheuscordeiro.libaryapi.api.dto.LoanDTO;
+import com.matheuscordeiro.libaryapi.model.entity.Book;
+import com.matheuscordeiro.libaryapi.model.entity.Loan;
+import com.matheuscordeiro.libaryapi.service.BookService;
+import com.matheuscordeiro.libaryapi.service.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class LoanController {
+    private final LoanService loanService;
+    private final BookService bookService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody LoanDTO dto) {
+        Book book = bookService.getBookByIsbn(dto.getIsbn()).get();
+        Loan entity = Loan.builder()
+                .book(book)
+                .customer(dto.getCostumer())
+                .loanDate(LocalDate.now())
+                .build();
+        entity = loanService.save(entity);
+        return entity.getId();
+    }
 }
