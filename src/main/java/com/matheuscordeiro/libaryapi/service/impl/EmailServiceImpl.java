3af58707ce2ffa2d8bee2fae7c70efd96c4cd721
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-30 17:36:39 -0300

    Mail trap config

---
+++ src/main/java/com/matheuscordeiro/libaryapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.matheuscordeiro.libaryapi.service.impl;
+
+import com.matheuscordeiro.libaryapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+    @Override
+    public void sendMail(String message, List<String> mailsList) {
+        
+    }
+}
